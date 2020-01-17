ee8f0470d77558bb25b76df97a354a73d6d0884e
pottsen <peter.k.ottsen@gmail.com>
2020-01-17 07:21:56 -0700

    added checks to make sure file max and point max are the same

--- algorithms/grid_rev.py
+++ algorithms/grid_rev.py
@@ -92,39 +92,83 @@ class Grid():
         # self.make_kd_tree()
         
     def make_grid_by_cell(self, size_of_cells):
+        
+        # Find out what the point format looks like.
+        # print("point format")
+        # pointformat = self.base_file.point_format
+        # for spec in pointformat:
+        #     print(spec.name)
+
+        # # #Like XML or etree objects instead?
+        # # a_mess_of_xml = pointformat.xml()
+        # # an_etree_object = pointformat.etree()
+
+        # # #It looks like we have color data in this file, so we can grab:
+        # # blue = inFile.blue
+        # print("\nheader format")
+        # #Lets take a look at the header also.
+        # headerformat = self.base_file.header.header_format
+        # for spec in headerformat:
+        #     print(spec.name)
+
+        # print(self.base_file.header.max)
+        # print(self.base_file.header.min)
+        
         #################################################
         #pull in the base x array and base y array -- max. deltas, and then assigning points to grid cells
-        base_x = self.base_file.X
+        #### USE LOWER CASE x, y, z on self.base_file
+        base_x = self.base_file.x
         max_x = np.max(base_x)
-        print("max x ", max_x)
         min_x = np.min(base_x)
+
+        print("max x ", max_x)
+        if round(max_x,2) != round(self.base_file.header.max[0], 2):
+            print("x max coordinate mismatch", max_x, self.base_file.header.max[0])
         print("min x ", min_x)
+        if round(min_x,2) != round(self.base_file.header.min[0], 2):
+            print("x min coordinate mismatch")
 
-        base_y = self.base_file.Y
+        base_y = self.base_file.y
         max_y = np.max(base_y)
-        print("max y ", max_y)
         min_y = np.min(base_y)
+
+        print("max y ", max_y)
+        if round(max_y,2) != round(self.base_file.header.max[1], 2):
+            print("y max coordinate mismatch")
         print("min y ", min_y)
+        if round(min_y,2) != round(self.base_file.header.min[1], 2):
+            print("y min coordinate mismatch")
 
-        base_z = self.base_file.Z
+        base_z = self.base_file.z
+        max_z = np.max(base_z)
+        min_z = np.min(base_z)
 
+        print("max z ", max_z)
+        if round(max_z,2) != round(self.base_file.header.max[2], 2):
+            print("y max coordinate mismatch")
+        print("min z ", min_z)
+        if round(min_z,2) != round(self.base_file.header.min[2], 2):
+            print("y min coordinate mismatch")
+
+        
         ################################################
         # calculate x and y length of scan to be used in determing grid spots
         delta_x = abs(max_x - min_x)
         delta_y = abs(max_y - min_y)
+        area = float(delta_x)*float(delta_y)
 
         print("delta x", delta_x)
-        print("delta y", delta_y)
-
-        area = float(delta_x)*float(delta_y)
+        print("delta y", delta_y) 
         print("area", area)
 
         #################################################
         # number of cells for gridding = delta / size_of_cell
         # cieling used so that we dont cut off the end of the grid/scan
         print("Size of cells ", size_of_cells, " by ", size_of_cells)
+
         number_of_cells_x = math.ceil(delta_x/size_of_cells)
         print("# x cells", number_of_cells_x)
+
         number_of_cells_y = math.ceil(delta_y/size_of_cells)
         print("# y cells", number_of_cells_y)
 
@@ -166,7 +210,9 @@ class Grid():
         print('max number of points ', max_points)
         print("Total points in grid cells", total)
 
-        self.grid[max_i][max_j].find_vegetation(100)
+        self.grid[max_i][max_j].find_vegetation(10)
+       
+ 
         
         """
         # x_pointer = min_x + grid_x_size/2
@@ -202,13 +248,19 @@ class Grid():
         """
 
 
-    def make_kd_tree(self):
-        self.grid_cell_tree = spatial.cKDTree(self.mid_x_array)
+    # def make_kd_tree(self):
+    #     self.grid_cell_tree = spatial.cKDTree(self.mid_x_array)
 
 
         
+###################################################
+# # Ubuntu
+# grid = Grid("../../las_data/points_clean.las", 500)
 
-grid = Grid("../../las_data/points_clean.las", 500)
+###################################################
+# # Windows
+# grid = Grid("../../../Documents/YC_LiftDeck_10Dec19.las", 100)
+grid = Grid("../../../Documents/LiftDeck2.las", 1)
 
 
 
