4b90996c636c4366a3cdf389afc77d566fc09cab
pottsen <peter.k.ottsen@gmail.com>
2020-09-25 11:52:42 -0600

    alignemnt and snow depth tabs made in the master window

--- viewer/master_window.py
+++ viewer/master_window.py
@@ -1,7 +1,11 @@
 import window as SDW
 import ICP_window as AlgnW
+from PyQt5 import QtWidgets, QtCore, QtGui
+from canvas import Canvas
+import vispy.app
+import sys
 
-class Window(QMainWindow):
+class Window(QtWidgets.QMainWindow):
     # resize = pyqtSignal()
     def __init__(self):
         super(Window, self).__init__()
@@ -12,7 +16,7 @@ class Window(QMainWindow):
         self.snow_depth_window = SDW.Window()
         self.alignment_window = AlgnW.Window()
 
-        self.window_widgets = QTabWidget()
+        self.window_widgets = QtWidgets.QTabWidget()
         self.window_widgets.addTab(self.alignment_window, "Alignment")
         self.window_widgets.addTab(self.snow_depth_window, "Snow Depth")
         self.setCentralWidget(self.window_widgets)
